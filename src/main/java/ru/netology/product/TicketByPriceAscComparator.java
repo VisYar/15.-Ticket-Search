4cf60254bc51c016cc31ca392cfbac8306bd6aac
Tatyana Dmitrieva <visyar76@gmail.com>
2022-07-31 23:11:16 +0300

    Added ticket comparator

---
+++ src/main/java/ru/netology/product/TicketByPriceAscComparator.java
@@ -0,0 +1,12 @@
+package ru.netology.product;
+
+import java.util.Comparator;
+
+public class TicketByPriceAscComparator implements Comparator<Ticket> {
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        return 0;
+    }
+}
+
+
